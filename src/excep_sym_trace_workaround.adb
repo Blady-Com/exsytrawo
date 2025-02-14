cfc8851c9eebb84d817a088acb2597f9add28747
Blady <blady.net@orange.fr>
2025-02-14 21:15:29 +0100

    Reformat.

--- src/excep_sym_trace_workaround.adb
+++ src/excep_sym_trace_workaround.adb
@@ -8,8 +8,7 @@ with Ada.Strings.Unbounded.Text_IO;
 package body Excep_Sym_Trace_Workaround is
 
    function Exception_Information_Workaround
-     (Address_List : Ada.Strings.Unbounded.Unbounded_String)
-      return         String
+     (Address_List : Ada.Strings.Unbounded.Unbounded_String) return String
    is
       use type Ada.Strings.Unbounded.Unbounded_String;
       use type GNAT.OS_Lib.Argument_List;
@@ -23,11 +22,9 @@ package body Excep_Sym_Trace_Workaround is
       Ind      : Natural;
 
       Args : GNAT.OS_Lib.Argument_List_Access :=
-         GNAT.OS_Lib.Argument_String_To_List
-           ("-o " &
-            Ada.Command_Line.Command_Name &
-            " -arch x86_64 " &
-            Ada.Strings.Unbounded.To_String (Address_List));
+        GNAT.OS_Lib.Argument_String_To_List
+          ("-o " & Ada.Command_Line.Command_Name & " -arch x86_64 " &
+           Ada.Strings.Unbounded.To_String (Address_List));
    begin
       if GNAT.OS_Lib.Is_Executable_File (Exe) then
          Ada.Text_IO.Create (File, Ada.Text_IO.In_File, Filename);
@@ -58,33 +55,31 @@ package body Excep_Sym_Trace_Workaround is
             return Ada.Strings.Unbounded.To_String (Address_List);
          end if;
       else
-         return "Executable '" &
-                Exe &
-                "' not found, probably called from JAVA context.";
+         return
+           "Executable '" & Exe &
+           "' not found, probably called from JAVA context.";
       end if;
    end Exception_Information_Workaround;
 
    function Symbolic_Traceback
-     (Traceback : Ada.Exceptions.Traceback.Tracebacks_Array)
-      return      String
+     (Traceback : Ada.Exceptions.Traceback.Tracebacks_Array) return String
    is
       Address_List : Ada.Strings.Unbounded.Unbounded_String;
    begin
       if Traceback'Length /= 0 then
          for Ind in Traceback'Range loop
             Ada.Strings.Unbounded.Append
-              (Address_List,
-               " 0x" & System.Address_Image (Traceback (Ind)));
+              (Address_List, " 0x" & System.Address_Image (Traceback (Ind)));
          end loop;
          return Exception_Information_Workaround (Address_List);
       else
-         return "No traceback (possible missing of binder switch -E to store tracebacks in exception occurrences).";
+         return
+           "No traceback (possible missing of binder switch -E to store tracebacks in exception occurrences).";
       end if;
    end Symbolic_Traceback;
 
    function Symbolic_Traceback
-     (E    : Ada.Exceptions.Exception_Occurrence)
-      return String
+     (E : Ada.Exceptions.Exception_Occurrence) return String
    is
    begin
       return Symbolic_Traceback (Ada.Exceptions.Traceback.Tracebacks (E));
